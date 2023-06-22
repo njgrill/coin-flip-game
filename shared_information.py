3e3d4eff8818996015e541b1f045a7e4164650b9
Berke Lunstad <berkelunstad@optiver.us>
2023-06-22 12:49:58 -0500

    Updated locking for decision tree

--- shared_information.py
+++ shared_information.py
@@ -1,5 +1,8 @@
 import threading
-from sklearn import tree
+from typing import Union
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.base import clone
 
 class SharedInformation:
     def __init__(self):
@@ -10,8 +13,8 @@ class SharedInformation:
         self.queue_outputs = []
         self._queue_lock = threading.Lock()
 
-        self.model_training = None
-        self.model_trained = None
+        self.model_trained: DecisionTreeRegressor = DecisionTreeRegressor()
+        self.model_outdated: bool = False
         self._model_lock = threading.Lock()
 
     def add_to_queue(self, input, output):
@@ -26,6 +29,13 @@ class SharedInformation:
             self.queue_inputs = []
             self.queue_outputs = []
 
-    def copy_decision_tree(self):
+    def copy_decision_tree(self, model_trained):
         with self._model_lock:
+            self.model_trained = clone(model_trained)
+            self.model_outdated = True
 
+    def get_decision_tree(self) -> tuple[bool, Union[None, DecisionTreeRegressor]]:
+        with self._model_lock:
+            if not self.model_outdated:
+                return False, None
+            return True, self.model_trained
